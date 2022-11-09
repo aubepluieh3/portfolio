18fc5c26a27e55deae8013c8298779ce07a19d0d
soo <aubepluieh3@naver.com>
2022-11-09 23:54:06 +0900

    Email&GitHub

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleDown } from "@fortawesome/free-regular-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
 
 const Circle = styled.div`
   display: flex;
@@ -27,6 +28,7 @@ const ProfileBox = styled.div`
   border-radius: 30px;
   height: 300px;
   box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 50px;
+  padding: 15px;
 `;
 const Img = styled.div`
   img {
@@ -41,6 +43,30 @@ const Img = styled.div`
   margin-right: 10px;
 `;
 
+const Name = styled.span`
+  font-size: 30px;
+  font-weight: 600;
+  margin-bottom: 10px;
+`;
+
+const Introduce = styled.span`
+  font-size: 20px;
+  height: 200px;
+`;
+
+const Info = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  font-size: 30px;
+  a {
+    text-decoration-line: none;
+    margin-right: 5px;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+`;
 function profile() {
   return (
     <>
@@ -51,7 +77,19 @@ function profile() {
         <Img>
           <img src={`img/soo.jpg`} />
         </Img>
-        <ProfileBox></ProfileBox>
+        <ProfileBox>
+          <Name>👩🏻Park Soo Hyun </Name>
+          <Introduce></Introduce>
+          <Info>
+            <a href="mailto:aubepluieh3@naver.com">📧</a>
+            <FontAwesomeIcon
+              icon={faGithub}
+              onClick={() => {
+                window.open("https://github.com/aubepluieh3");
+              }}
+            />
+          </Info>
+        </ProfileBox>
       </Box>
       <Box>
         <ProfileBox></ProfileBox>
